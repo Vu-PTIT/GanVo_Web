4a712f1a7a41c69e4d52a06bc9a3bf79c7a5ff10
lamanh <lamanhlc2006@gmail.com>
2025-10-18 22:46:34 +0700

    cong form dang nhap

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import './assets/css/asset-sm.css';
 import './assets/css/asset.css';
 import './assets/css/index.css';
-import {Sign_up} from './page/sign-up';
+import {Sign_in} from './page/sign-in';
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Sign_up />
+    <Sign_in />
   </StrictMode>,
 )
---
+++ src/page/sign-in.tsx
@@ -0,0 +1,27 @@
+import '../assets/css/index.css';
+import '../assets/css/asset.css';
+import '../assets/css/sign-in.css';
+
+export function Sign_in(){
+    return(
+        <>
+            <div className="cont-sign-in">
+                <div className="content-sign-in">
+                    <h1 className="title-text mb-15">Đăng ký tài khoản</h1>
+                    <p className='title-text-under mb-25'>Tạo tài khoản mới để bắt đầu trải nghiệm dịch vụ của chúng tôi.</p>
+                    <div className="input-form-box">
+                        <label>Email</label>
+                        <input type='email' placeholder='your@example.com'></input>
+                    </div>
+                    <div className="input-form-box">
+                        <label>Mật khẩu</label>
+                        <input type='password' placeholder='Nhập mật khẩu'></input>
+                    </div>
+                    <p className='title-text-under mb-10'><span className='cl-blue'>Quên mật khẩu ?</span></p>
+                    <button className='btn-sign-in-up mb-35'>Đăng nhập</button>
+                    <p className='title-text-under m-0'>Chưa có tài khoản?<span className='cl-blue'> Đăng kí</span></p>
+                </div>
+            </div>
+        </>
+    )
+}

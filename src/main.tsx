0e3051ac5fbde631f09c3a4122b6c3d8e57c2592
lamanh <lamanhlc2006@gmail.com>
2025-10-24 14:28:39 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import './assets/css/asset-sm.css';
 import './assets/css/asset.css';
 import './assets/css/index.css';
+import {Appointment} from './page/appointment';
 import {Header} from './assets/components/header';
 import {Footer} from './assets/components/footer';
 import ReactDOM from "react-dom/client";
@@ -9,6 +10,7 @@ import { StrictMode } from "react";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
+    <Appointment />
     <Footer />
   </StrictMode>,
 )

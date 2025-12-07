c4fb8ed9d99f54afd7b2bf805202c8349c94bd40
Vu-PTIT <vu.ptit@gmail.com>
2025-12-08 00:38:20 +0700

    sua profile

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,17 +1,19 @@
 import '../assets/css/index.css';
 import '../assets/css/asset.css';
-import {Menu} from '../components/auth/menu/menu';
+import { Menu } from '../components/auth/menu/menu';
 import { Header } from '../components/auth/header';
 import { ProfileForm } from '../components/auth/profile-form/profile-form';
 
-export function Profile(){
-    return(
+export function Profile() {
+    return (
         <div className="layout">
             <Header />
             <main id="profile">
-                <div className="profile scoll-auto">
+                <div className="profile profile-page-correction">
                     <Menu />
-                    <ProfileForm />
+                    <div className="profile-scroll-container">
+                        <ProfileForm />
+                    </div>
                 </div>
             </main>
         </div>

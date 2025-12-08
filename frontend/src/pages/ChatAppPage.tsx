b3352aef05c628fdade87f9012b4ac63a8683eb4
Vu-PTIT <vu.ptit@gmail.com>
2025-12-08 15:05:45 +0700

    tao chat khi match

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -2,7 +2,9 @@ import '../assets/css/index.css';
 import '../assets/css/asset.css';
 import './ChatAppPage.css';
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useChatStore } from "@/stores/useChatStore";
+import { useConversationStore } from '@/stores/useConversationStore';
 import ConversationList from "@/components/chat/ConversationList";
 import ChatWindow from "@/components/chat/ChatWindow";
 import { Menu } from '../components/auth/menu/menu';
@@ -11,10 +13,29 @@ import { Header } from '../components/auth/header';
 const ChatAppPage = () => {
   const { connectSocket, disconnectSocket } = useChatStore();
 
+  const { getOrCreateDirectConversation } = useConversationStore();
+  const { setSelectedConversationId } = useChatStore();
+  const [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
     connectSocket();
     return () => disconnectSocket();
   }, [connectSocket, disconnectSocket]);
+
+  useEffect(() => {
+    const userId = searchParams.get("userId");
+    if (userId) {
+      const initChat = async () => {
+        const conversationId = await getOrCreateDirectConversation(userId);
+        if (conversationId) {
+          setSelectedConversationId(conversationId);
+          // Clear param
+          setSearchParams({});
+        }
+      };
+      initChat();
+    }
+  }, [searchParams, getOrCreateDirectConversation, setSelectedConversationId, setSearchParams]);
 
   return (
     <div className="layout">
